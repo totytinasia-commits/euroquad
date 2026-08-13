cbde25c6e42ceb15f9b014939209b144fbb404cc
totytinasia-commits <totytinasia@gmail.com>
2026-08-13 15:30:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ elif page == "Risultati Giocatore":
     # --- Lobby 1 ---
     st.markdown("<h3 style='text-align: center;'>Lobby 1</h3>", unsafe_allow_html=True)
     try:
-        lobby1_df = df_player.iloc[11:35, cols].copy()
+        lobby1_df = df_player.iloc[11:39, cols].copy()
         lobby1_df.columns = headers_player
         st.markdown(render_custom_table(lobby1_df, headers_player), unsafe_allow_html=True)
     except Exception as e:
@@ -420,7 +420,7 @@ elif page == "Risultati Giocatore":
     # --- Lobby 2 ---
     st.markdown("<h3 style='text-align: center;'>Lobby 2</h3>", unsafe_allow_html=True)
     try:
-        lobby2_df = df_player.iloc[41:65, cols].copy()
+        lobby2_df = df_player.iloc[43:70, cols].copy()
         lobby2_df.columns = headers_player
         st.markdown(render_custom_table(lobby2_df, headers_player), unsafe_allow_html=True)
     except Exception as e:
@@ -429,7 +429,7 @@ elif page == "Risultati Giocatore":
     # --- Lobby 3 ---
     st.markdown("<h3 style='text-align: center;'>Lobby 3</h3>", unsafe_allow_html=True)
     try:
-        # AGGIORNA GLI INDICI QUI SOTTO (ESEMPIO: 71:95) IN BASE A DOVE SI TROVA LA LOBBY 3 NEL FOGLIO
+        # AGGIORNA GLI INDICI QUI SOTTO (ESEMPIO: 74:100) IN BASE A DOVE SI TROVA LA LOBBY 3 NEL FOGLIO
         lobby3_df = df_player.iloc[71:95, cols].copy() 
         lobby3_df.columns = headers_player
         st.markdown(render_custom_table(lobby3_df, headers_player), unsafe_allow_html=True)
